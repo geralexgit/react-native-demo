1d4c2d3954fe48669ff92b81655d50011d492ef6
Alexander Gerasimov <A.Gerasimov@stream.ru>
2020-11-11 16:56:47 +0300

    adaptivity with Dimension class addded

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Dimensions } from 'react-native'
 import { FontAwesome, AntDesign } from '@expo/vector-icons'
 import { AppCard } from '../components/AppCard/AppCard'
 import { AppTextBold } from '../components/AppTextBold/AppTextBold'
@@ -55,7 +55,8 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   button: {
-    width: '40%',
+    // width: Dimensions.get('window').width / 3,
+    width: Dimensions.get('window').width > 400 ? 150 : 100,
   },
   title: {
     fontSize: 20,
