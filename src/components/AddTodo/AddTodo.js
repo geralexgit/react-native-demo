700e62a8fd40fa441ecb17f63f1d2b1dbb7636a8
Alexander Gerasimov <A.Gerasimov@stream.ru>
2020-11-11 16:48:03 +0300

    OS check added

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, TextInput, Button, Alert } from 'react-native'
+import { View, StyleSheet, TextInput, Alert, Keyboard } from 'react-native'
 import { THEME } from '../../theme'
 import { AntDesign } from '@expo/vector-icons'
 
@@ -9,6 +9,7 @@ export const AddTodo = ({ onSubmit }) => {
     if (value.trim()) {
       onSubmit(value)
       setValue('')
+      Keyboard.dismiss()
     } else {
       Alert.alert('Empty input!')
     }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Platform } from 'react-native'
 import { THEME } from '../../theme'
 import { AppTextBold } from '../AppTextBold/AppTextBold'
 
 export const Navbar = (props) => {
   const { title } = props
   return (
-    <View style={styles.navbar}>
+    <View
+      style={{
+        ...styles.navbar,
+        ...Platform.select({
+          ios: styles.navbarIos,
+          android: styles.navbarAndroid,
+        }),
+      }}
+    >
       <AppTextBold style={styles.text}>{title}</AppTextBold>
     </View>
   )
@@ -17,11 +25,17 @@ const styles = StyleSheet.create({
     height: 70,
     alignItems: 'center',
     justifyContent: 'space-around',
-    backgroundColor: THEME.MAIN_COLOR,
     paddingBottom: 10,
   },
+  navbarAndroid: {
+    backgroundColor: THEME.MAIN_COLOR,
+  },
+  navbarIos: {
+    borderBottomColor: THEME.MAIN_COLOR,
+    borderBottomWidth: 1,
+  },
   text: {
-    color: 'white',
+    color: Platform.OS === 'ios' ? THEME.MAIN_COLOR : '#fff',
     fontSize: 20,
   },
 })
